73d0177ed135485c73c17ed366905e187c1b23d9
HY2DIE <tulushidayatullahbs@gmail.com>
2021-11-07 09:19:20 +0700

    oke

---
+++ chapter 1.py
@@ -0,0 +1,6 @@
+
+print('Do for this life as if you live forever,\n   do for the afterlife as if you die tomorrow.\n"Ali bin Abi Thalib"')
+
+
+print("Sanbercode","Python", sep = '--', end='__')
+print("Bootcamp")
