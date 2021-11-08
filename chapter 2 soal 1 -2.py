a59342cbe3e2ea71efd3db05653a4c79b1b5069c
HY2DIE <tulushidayatullahbs@gmail.com>
2021-11-08 11:14:50 +0700

    oke

---
+++ chapter 2 soal 1 -2.py
@@ -0,0 +1,28 @@
+# soal 1
+
+
+#jaminput = int(input("Mulai Acara Jam = "))
+#menitinput = int(input("Mulai Acara Menit = "))
+#durasiinput = int(input("Durasi Acara = "))
+#AcaraMulaijam = int(round(jaminput + durasiinput / 60))
+#AcaraMulaimenit= int((menitinput + durasiinput) % 60) 
+#print(f"Acara akan selesai pukul = {AcaraMulaijam}:{AcaraMulaimenit}")
+
+
+#soal 2
+
+#x = int(input("Masukan nilai X = "))
+#y = int(input("Masukan nilai Y = "))
+
+#fungsi = float((2*(x**3)*(y*2)) + ((x**3)*y) + (4*x*y) )
+
+#print(f"f(x,y) = {fungsi}")
+
+
+
+
+ 
+
+
+
+ 
